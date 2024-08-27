66c1e1e85d63027d3dd0ed50ec8ab11c8a71039a
Maria Paz <pazrodriguez612@gmail.com>
2024-08-27 16:14:28 -0700

    Updated ENV names

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -29,7 +29,7 @@ export const GET = async (
     return new NextResponse(JSON.stringify(product), {
       status: 200,
       headers: {
-        'Access-Control-Allow-Origin': `${process.env.ECOMMERCE_STORE_URL}`,
+        'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_API_STORE_URL}`,
         'Access-Control-Allow-Methods': 'GET',
         'Access-Control-Allow-Headers': 'Content-Type',
       },
